70f133fd46279438f73a254af39780aece463ee7
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-03-15 10:45:43 +0100

    style: change dropdown background color to #F4F1E8

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                             </div>
                             <ul
                                 tabIndex={0}
-                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow font-semibold">
+                                className="menu menu-sm dropdown-content bg-[#F4F1E8] rounded-box z-1 mt-3 w-52 p-2 shadow font-semibold">
                                 {links}
                             </ul>
                         </div>
@@ -43,7 +43,7 @@ const Navbar = () => {
                             </div>
                             <ul
                                 tabIndex={0}
-                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                className="menu menu-sm dropdown-content bg-[#F4F1E8] rounded-box z-1 mt-3 w-52 p-2 shadow">
                                 <li><a>Settings</a></li>
                                 <li><button onClick={() => logOut()}>Logout</button></li>
                             </ul>
